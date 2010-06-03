2513a8c47f5e9785abd21151162f3c51568363c3
Kevin Mudrick <kevin@mudrick.com>
2010-06-03 00:19:24 -0400

    Cleanup

--- src/test/java/com/mudrick/common/VersionUtilTest.java
+++ src/test/java/com/mudrick/common/VersionUtilTest.java
@@ -14,17 +14,17 @@ public class VersionUtilTest {
     assertTrue(VersionUtil.compare("1", "1") == 0);
     assertTrue(VersionUtil.compare("2", "1") > 0);
     assertTrue(VersionUtil.compare("1", "2") < 0);
-    VersionUtil.compare("1.0", "1");
-    VersionUtil.compare("1.0.0", "1");
-    VersionUtil.compare("1.0.1", "1");
-    VersionUtil.compare("3.1", "3.1.0");
-    VersionUtil.compare("3.1.0", "3.10");
-    VersionUtil.compare("3.1", "3.10");
-    VersionUtil.compare("3.1", "3.1.1");
-    VersionUtil.compare("3.1", "3.0.0.2");
-    VersionUtil.compare("3.1", "3.1.0.0.0.0.0.0");
-    VersionUtil.compare("3.1", "3.1.a");
-    VersionUtil.compare("3.1", "3.1a");
+    assertTrue(VersionUtil.compare("1.0", "1") == 0);
+    assertTrue(VersionUtil.compare("1.0.0", "1") == 0);
+    assertTrue(VersionUtil.compare("1.0.1", "1") > 0);
+    assertTrue(VersionUtil.compare("3.1", "3.1.0") == 0);
+    assertTrue(VersionUtil.compare("3.1.0", "3.10") < 0);
+    assertTrue(VersionUtil.compare("3.1", "3.10") < 0);
+    assertTrue(VersionUtil.compare("3.1", "3.1.1") < 0);
+    assertTrue(VersionUtil.compare("3.1", "3.0.0.2") > 0);
+    assertTrue(VersionUtil.compare("3.1", "3.1.0.0.0.0.0.0") == 0);
+    assertTrue(VersionUtil.compare("3.1", "3.1.a") == 0);
+    assertTrue(VersionUtil.compare("3.1", "3.1a") == 0);
   }
   
 }
